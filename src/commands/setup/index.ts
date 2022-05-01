258381fdc2719edacb1046c3fed73daa1939bc13
Lukas Mertens <git@lukas-mertens.de>
2022-05-01 19:10:17 +0200

    fix: migrated to cliux

--- src/commands/setup/index.ts
+++ src/commands/setup/index.ts
@@ -1,5 +1,4 @@
-import {Command, Flags} from '@oclif/core'
-import cli from 'cli-ux'
+import {Command, Flags, CliUx} from '@oclif/core'
 import {config, configDirectory, configPath, configType} from '../../config'
 import * as fs from 'fs'
 
@@ -19,11 +18,11 @@ export default class Setup extends Command {
 
     this.log(`Running setup...`)
     const newConfig: configType = {
-      defaultWorkTime: await cli.prompt('What should should be the default work time?', {default: config.defaultWorkTime.toString()}),
-      defaultBreakTime: await cli.prompt('What should be the default breaktime in minutes?', {default: config.defaultBreakTime.toString()}),
-      defaultName: await cli.prompt('What should should be your name?', {default: config.defaultName}),
-      defaultEmail: await cli.prompt('What should should be your email?', {default: config.defaultEmail}),
-      defaultSession: await cli.prompt('What should should be your default session?', {default: config.defaultSession}),
+      defaultWorkTime: await CliUx.ux.prompt('What should should be the default work time?', {default: config.defaultWorkTime.toString()}),
+      defaultBreakTime: await CliUx.ux.prompt('What should be the default breaktime in minutes?', {default: config.defaultBreakTime.toString()}),
+      defaultName: await CliUx.ux.prompt('What should should be your name?', {default: config.defaultName}),
+      defaultEmail: await CliUx.ux.prompt('What should should be your email?', {default: config.defaultEmail}),
+      defaultSession: await CliUx.ux.prompt('What should should be your default session?', {default: config.defaultSession}),
     };
     fs.mkdirSync(configDirectory, {recursive: true}),
     fs.writeFileSync(configPath, JSON.stringify(newConfig), {encoding: 'utf-8'});
